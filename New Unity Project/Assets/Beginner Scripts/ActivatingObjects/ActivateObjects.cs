302eaa951bfc1a2e8e6220a8b04e7a6f946f130e
R0GU3-0N3 <waltseiter@gmail.com>
2023-03-07 12:04:23 -0700

    Added remaining files from the Beginner Scripting tutorial

---
+++ New Unity Project/Assets/Beginner Scripts/ActivatingObjects/ActivateObjects.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateObjects : MonoBehaviour
+{
+    void Start ()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ New Unity Project/Assets/Beginner Scripts/Using Other Components/AComponent.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AComponent : MonoBehaviour
+{
+    public int playerScore = 9001;
+}
