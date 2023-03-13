bfc42599ab898f09f82b14ac0996d6533184aeea
R0GU3-0N3 <waltseiter@gmail.com>
2023-03-13 13:51:27 -0600

    Added FloatData tutorial files

---
+++ New Unity Project/Assets/ScriptTriggerEventBehavior.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ScriptTriggerEventBehavior : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+
+}
