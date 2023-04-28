61250e1d714b2f262aeb71f129ef95d9376abf23
R0GU3-0N3 <waltseiter@gmail.com>
2023-04-28 15:35:05 -0600

    Finished UDF

--- UFO Defence Force/Assets/Scripts/DestroyOutOfBounds.cs
+++ UFO Defence Force/Assets/Scripts/DestroyOutOfBounds.cs
@@ -7,12 +7,18 @@ public class DestroyOutOfBounds : MonoBehaviour
     public float topBounds = 30.0f;
 
     public float bottomBounds = -10.0f;
+
+    public GameManager gameManager;
  
     void Awake()
     {
         Time.timeScale = 1;
     }
-
+    
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
     void Update()
     {
         if(transform.position.z > topBounds)
@@ -21,9 +27,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z < bottomBounds)
         {
-            Debug.Log("Game Over!");
-            Destroy(gameObject);
-            Time.timeScale = 0;
+            gameManager.isGameOver = true;
         }
     }
 }
